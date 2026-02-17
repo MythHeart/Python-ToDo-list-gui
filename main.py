5ab3b128ac46836d14b2382db828fb9404bcc47e
Jacob Graver <jacobgraver@proton.me>
2026-02-17 13:53:34 -0700

    initial app window creation

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+from kivy.app import App
+from kivy.core.window import Window
+from widgets import TEAL, MainWindow
+
+Window.clearcolor = TEAL
+
+
+class TodoApp(App):
+    title = "Todo App"
+
+    def build(self):
+        return MainWindow()
+
+
+if __name__ == "__main__":
+    todoapp = TodoApp()
+    todoapp.run()
--- widgets.py
+++ widgets.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+from kivy.uix.floatlayout import FloatLayout
+
+TEAL = (0, 0.31, 0.31, 1.0)
+
+
+class MainWindow(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
